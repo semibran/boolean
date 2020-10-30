d7985d51ea920b010ea563ade18ced68736ace4c
Brandon Semilla <semibran@gmail.com>
2020-10-29 23:12:48 -0700

    print errors to stdout

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ let outputbox = document.querySelector(".output-box")
 let output = document.querySelector(".output")
 
 form.onsubmit = event => {
+	event.preventDefault()
 	let expr1 = input1.value
 	let expr2 = input2.value
-	let token1 = parse(expr1)
-	let token2 = parse(expr2)
+	let token1 = null
+	let token2 = null
+	try {
+		token1 = parse(expr1)
+	} catch (err) {
+		return console.log(colonslice(err.message))
+	}
+	try {
+		token2 = parse(expr2)
+	} catch (err) {
+		return console.log(colonslice(err.message))
+	}
 	let id1 = id(token1)
 	let id2 = id(token2)
 	if (id1 === id2) {
@@ -23,7 +34,14 @@ form.onsubmit = event => {
 		outputbox.classList.add("-incorrect", "-result")
 		output.innerHTML = expr1 + " &NotEqual; " + expr2
 	}
-	event.preventDefault()
+}
+
+function colonslice(str) {
+	for (var i = str.length; i;) {
+		if (str[--i] === ":") break
+	}
+	if (!i) return str
+	return str.slice(i + 2, str.length)
 }
 
 let year = document.querySelector("#year")
