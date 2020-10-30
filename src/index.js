24c3f30b5f0544b712a53f06b0d1e8daa3c07116
Brandon Semilla <semibran@gmail.com>
2020-10-29 23:06:29 -0700

    output

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import id from "./lib/bitstr"
 let form = document.querySelector(".section.-form")
 let input1 = document.querySelector("#expr1")
 let input2 = document.querySelector("#expr2")
+let outputbox = document.querySelector(".output-box")
+let output = document.querySelector(".output")
 
 form.onsubmit = event => {
-	let token1 = parse(input1.value)
-	let token2 = parse(input2.value)
+	let expr1 = input1.value
+	let expr2 = input2.value
+	let token1 = parse(expr1)
+	let token2 = parse(expr2)
 	let id1 = id(token1)
 	let id2 = id(token2)
-	console.log(id1, id2)
+	if (id1 === id2) {
+		outputbox.classList.remove("-incorrect")
+		outputbox.classList.add("-correct", "-result")
+		output.innerHTML = expr1 + " = " + expr2
+	} else {
+		outputbox.classList.remove("-correct")
+		outputbox.classList.add("-incorrect", "-result")
+		output.innerHTML = expr1 + " &NotEqual; " + expr2
+	}
 	event.preventDefault()
 }
 
